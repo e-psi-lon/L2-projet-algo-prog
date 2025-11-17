a99879243f28fdbd7b8febdd1e980ff82232a3ad
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-11-17 12:38:49 +0100

    chore: add example configuration files and add gradle run entrypoint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "io.github.e_psi_lon"
@@ -10,6 +11,10 @@ repositories {
 }
 
 dependencies {
+    // Database - SQLite for development, MySQL for final
+    implementation("org.xerial:sqlite-jdbc:3.51.0.0")
+    implementation("com.mysql:mysql-connector-j:9.5.0")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -18,3 +23,8 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+application {
+    mainClass.set("io.github.e_psi_lon.wordcrafter.WordCrafterApp")
+}
+
